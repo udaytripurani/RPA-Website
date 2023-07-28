0a4a1d592cc0d139ce8dbc8deb10540364535adc
Thanmai <2100031107@kluniversity.in>
2023-07-28 17:36:13 +0530

    added navbar and home page

---
+++ project/src/home.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import './home.css';
+function Home() {
+  return (
+    <div>
+     
+      <div id="container">
+        <h1><center>RPA- ROBOTIC PROCESS AUTOMATION</center></h1>
+        <h3>WHY RPA?</h3>
+        <p>
+          Usually, everyone is curious to get their process done so easily without human interference which saves them time
+          and reduces workload so that it makes life rest assured, have you ever think of that? and When you automate mundane
+          tasks throughout your workday, it makes your career that much more fulfilling. But where do you go to learn how to
+          automate things on your own? Sometimes when learning a new skill, it is easiest to watch an expert performing his
+          or her craft while explaining the process. So, yes, we have got a platform here to exhibit our talent through
+          Automation Anywhere Tool, UiPath, Blue Prism, etc. which helps us learn these automations fast and easy.
+        </p>
+
+        <h3>ESTABLISHMENT</h3>
+        <p>
+          This RPA club is an association of people united by a common interest and goal because in this technical world,
+          everyone feels like coding is the best way to build a tool, automate process and solve real-world problems. Yes, it
+          is true but there are various ways other than coding for people to not restrict to a particular area of development.
+          This is the best thing happened, students started to get much involved in these tools of RPA like UiPath and
+          Automation Anywhere which paved a way to get situations less hard for our Team. So, starting to begin with an idea
+          of forming this club there came many hurdles questioning the future of RPA. Also, we require plenty of resources,
+          references and professionals to guide to get practiced and get habituated to the tools we learn. After got prepared
+          and by trials, finally we brainstormed and came up with a vision that this club will help students learn the process
+          of automating things, its main area of use and the solutions to solve real-world issues, also making every process
+          costs lesser time and man-force to get out of things so well. A day 26th JANUARY,2020 enlightened for the heading
+          success of this RPA Club. Hard work added with Smartness helped a lot to discuss among us and form a team of 15 with
+          an excellent Program Chief, we got ourselves excellently trained from the start. And there comes the most powerful
+          Club-RPA, the journey continued...
+        </p>
+      </div>
+      </div>
+
+ 
+  );
+}
+
+export default Home;
---
+++ project/src/nav.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './nav.css';
+const Nav = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <img src={"/images/logo.png"} />
+      </div>
+      <ul className="navbar-components">
+        <li>
+          <a href="#">EVENTS</a>
+        </li>
+        <li>
+          <a href="#">BLOGS</a>
+        </li>
+        <li>
+          <a href="#">GALLERY</a>
+        </li>
+        <li>
+          <a href="#">OUR TEAM</a>
+        </li>
+      </ul>
+      <button className="button-50 login-button" role="button">
+        LOGIN
+      </button>
+    </nav>
+  );
+};
+
+export default Nav;
